9d7eaa874237d102018ec3324f02af54a9dd7dfd
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-01 10:58:52 -0300

    Refatorando codigo - quebrando em funções menores uma grande no form

--- JavaScript: Programando na linguagem da Web/Modulo05/assets/js/form.js
+++ JavaScript: Programando na linguagem da Web/Modulo05/assets/js/form.js
@@ -14,41 +14,66 @@ botaoAdicionaPaciente.addEventListener("click", (event)=>{
 
   let form  = document.querySelector("#form-adiciona");
 
-  // Guarda as informações do formulario
-  let nome = form.nome.value;
-  let peso = form.peso.value;
-  let altura = form.altura.value;
-  let gordura = form.gordura.value;
+  // Chamando função que extrai os dados do paciente e salva no objeto paciente
 
-  // Cria uma nova tr com suas td's
-  let criaPaciente = document.createElement("tr");
+  let paciente = extraiDadosForm(form);
 
-  let nomeTd = document.createElement("td");
-  let pesoTd = document.createElement("td");
-  let alturaTd = document.createElement("td");
-  let gorduraTd = document.createElement("td");
-  let imcTd = document.createElement("td");
+  // Chama a função que cria a nova tabela paciente
 
-  // Insere os valores nos campos criados
-
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura;
-  imcTd.textContent = calculaImc(peso, altura);
-
-  // Amarra a tr os td's criados
-
-  criaPaciente.appendChild(nomeTd);
-  criaPaciente.appendChild(pesoTd);
-  criaPaciente.appendChild(alturaTd);
-  criaPaciente.appendChild(gorduraTd);
-  criaPaciente.appendChild(imcTd);
-
+  let pacienteCriado = criaTabelaPaciente(paciente);
+ 
   // Momento de inserir a tabela criada na tabela de pacientes
 
   let tabela = document.querySelector("#tabela-pacientes");
 
-  tabela.appendChild(criaPaciente);
+  tabela.appendChild(pacienteCriado);
 
 }); //pode receber uma função anonima ou nomeada (externa);
+
+
+function extraiDadosForm(form){
+
+  let paciente = {
+    nome: form.nome.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calculaImc(form.peso.value, form.altura.value)
+
+  }
+
+  return paciente;
+}
+
+function criaTabelaPaciente(paciente) {
+
+
+   // Cria uma nova tr com suas td's
+
+   let criaPaciente = document.createElement("tr");
+
+   let nomeTd = criaPropriedaTabelaPaciente(paciente.nome, "info-nome");
+   let pesoTd = criaPropriedaTabelaPaciente(paciente.peso, "info-peso");
+   let alturaTd = criaPropriedaTabelaPaciente(paciente.altura, "info-altura");
+   let gorduraTd = criaPropriedaTabelaPaciente(paciente.gordura, "info-gordura");
+   let imcTd = criaPropriedaTabelaPaciente(paciente.imc, "info-imc");
+ 
+   // Amarra a tr os td's criados
+ 
+   criaPaciente.appendChild(nomeTd);
+   criaPaciente.appendChild(pesoTd);
+   criaPaciente.appendChild(alturaTd);
+   criaPaciente.appendChild(gorduraTd);
+   criaPaciente.appendChild(imcTd);
+
+  return criaPaciente;
+}
+
+function criaPropriedaTabelaPaciente(dado, classe) {
+  let td = document.createElement("td");
+
+  td.textContent = dado; // Insere o valor na tabela
+  td.classList.add(classe); // Coloca uma classe a aquele valor
+
+  return td;
+}
