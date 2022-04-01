cd8fa680fa0972172d8588fdb91245af01f93d6a
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-01 09:46:54 -0300

    Inserção de pacientes na tabela implementado

--- JavaScript: Programando na linguagem da Web/Modulo04/assets/js/scripts.js
+++ JavaScript: Programando na linguagem da Web/Modulo04/assets/js/scripts.js
@@ -6,6 +6,8 @@
 - .querySelectorAll retorna um Array!!!
 - .classList retorna todas as classes daquele elemento (possui metodos)
 
+- .preventDefault não deixa o botao executar seu comportamento padrao (limpar o formulario (quando não se tem destino os dados) e recarregar a pagina)
+
 */
 
 /* Troca Titulo */
@@ -57,9 +59,54 @@ for(i = 0; i < pacientes.length; i++){
 
     let imcResultadoPaciente = imc(pesoPacienteConvertido, alturaPacienteContertido);
     
-    imcResultadoPaciente = imcResultadoPaciente.toFixed(2);
-
-    imcPaciente.textContent = imcResultadoPaciente; // injeta no HTML o IMC
+    imcPaciente.textContent = imcResultadoPaciente.toFixed(2); // injeta no HTML o IMC
   }
   
 }
+
+/* Adiciona novo paciente */
+
+let botaoAdicionaPaciente = document.querySelector("#adicionar-paciente");
+
+botaoAdicionaPaciente.addEventListener("click", (event)=>{
+
+  event.preventDefault(); 
+
+  let form  = document.querySelector("#form-adiciona");
+
+  // Guarda as informações do formulario
+  let nome = form.nome.value;
+  let peso = form.peso.value;
+  let altura = form.altura.value;
+  let gordura = form.gordura.value;
+
+  // Cria uma nova tr com suas td's
+  let criaPaciente = document.createElement("tr");
+
+  let nomeTd = document.createElement("td");
+  let pesoTd = document.createElement("td");
+  let alturaTd = document.createElement("td");
+  let gorduraTd = document.createElement("td");
+
+  // Insere os valores nos campos criados
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  // Amarra a tr os td's criados
+
+  criaPaciente.appendChild(nomeTd);
+  criaPaciente.appendChild(pesoTd);
+  criaPaciente.appendChild(alturaTd);
+  criaPaciente.appendChild(gorduraTd);
+
+  // Momento de inserir a tabela criada na tabela de pacientes
+
+  let tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(criaPaciente);
+
+}); //pode receber uma função anonima ou nomeada (externa);
+
