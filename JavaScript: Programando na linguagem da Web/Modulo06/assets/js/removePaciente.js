92403dc389e133e2b7e3a58d09f7145f137d4fdb
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-01 14:23:15 -0300

    Implementado a animação de remoção do paciente

--- JavaScript: Programando na linguagem da Web/Modulo06/assets/js/removePaciente.js
+++ JavaScript: Programando na linguagem da Web/Modulo06/assets/js/removePaciente.js
@@ -1,10 +1,11 @@
-let tabelaPacientes = document.querySelectorAll(".paciente")
+let tabelaPacientes = document.querySelectorAll(".paciente");
 
 tabelaPacientes.forEach((paciente) => {
-    paciente.addEventListener("dblclick", (event)=>{        
+  paciente.addEventListener("dblclick", (event) => {
+    event.target.parentNode.classList.add("fadeOut");
 
-    event.target.parentNode.remove(); // O target pega exatamente onde ocorreu o evento (de duplo click) e com isso usamos o parentNode (para chegar ao pai (td))
-
-    }); 
-
-})
+    setTimeout(() => {
+      event.target.parentNode.remove(); // O target pega exatamente onde ocorreu o evento (de duplo click) e com isso usamos o parentNode (para chegar ao pai (td))
+    }, 500);
+  });
+});
