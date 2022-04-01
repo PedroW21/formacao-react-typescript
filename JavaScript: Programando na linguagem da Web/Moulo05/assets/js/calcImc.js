635f8eebf6614d9fb9a7c8d2b0147d8539bcd548
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-01 10:11:39 -0300

    Boa pratica: separando arquivos e suas funcionalidades

--- JavaScript: Programando na linguagem da Web/Moulo05/assets/js/calcImc.js
+++ JavaScript: Programando na linguagem da Web/Moulo05/assets/js/calcImc.js
@@ -64,49 +64,4 @@ for(i = 0; i < pacientes.length; i++){
   
 }
 
-/* Adiciona novo paciente */
-
-let botaoAdicionaPaciente = document.querySelector("#adicionar-paciente");
-
-botaoAdicionaPaciente.addEventListener("click", (event)=>{
-
-  event.preventDefault(); 
-
-  let form  = document.querySelector("#form-adiciona");
-
-  // Guarda as informações do formulario
-  let nome = form.nome.value;
-  let peso = form.peso.value;
-  let altura = form.altura.value;
-  let gordura = form.gordura.value;
-
-  // Cria uma nova tr com suas td's
-  let criaPaciente = document.createElement("tr");
-
-  let nomeTd = document.createElement("td");
-  let pesoTd = document.createElement("td");
-  let alturaTd = document.createElement("td");
-  let gorduraTd = document.createElement("td");
-
-  // Insere os valores nos campos criados
-
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = altura;
-  gorduraTd.textContent = gordura;
-
-  // Amarra a tr os td's criados
-
-  criaPaciente.appendChild(nomeTd);
-  criaPaciente.appendChild(pesoTd);
-  criaPaciente.appendChild(alturaTd);
-  criaPaciente.appendChild(gorduraTd);
-
-  // Momento de inserir a tabela criada na tabela de pacientes
-
-  let tabela = document.querySelector("#tabela-pacientes");
-
-  tabela.appendChild(criaPaciente);
-
-}); //pode receber uma função anonima ou nomeada (externa);
 
