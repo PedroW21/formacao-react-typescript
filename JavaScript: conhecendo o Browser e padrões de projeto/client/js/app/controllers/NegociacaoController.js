da31a92e9d798148cf8413bdb81d9f02a2f17773
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-02 20:02:36 -0300

    Complicando descomplicadamente uma data

--- JavaScript: conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
+++ JavaScript: conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
@@ -13,8 +13,13 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(this._inputData.value.split("-"));
-        console.log(data);
+        let data = new Date(
+
+            ...this._inputData.value
+            .split("-")
+            .map((item, indice)=>item - indice % 2)
+
+            );
 
         let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
 
