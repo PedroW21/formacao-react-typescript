776888f21e1186e5581d33e12fa35854aca8bbab
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-02 19:43:13 -0300

    Arrumado a criacação da negociação - data estava com problema

--- JavaScript: conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
+++ JavaScript: conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoController.js
@@ -4,16 +4,22 @@ class NegociacaoController {
 
         let $ = document.querySelector.bind(document);
 
-        this.inputData = $("#data")
-        this.inputQuantidade = $("#quantidade");
-        this.inputValor = $("#valor");
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
 
     }
 
     adiciona(event) {
         event.preventDefault();
 
+        let data = new Date(this._inputData.value.split("-"));
+        console.log(data);
 
+        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
 
+        console.log(negociacao);
+
+        // adicionar a negociação em uma lista
     }
 }
