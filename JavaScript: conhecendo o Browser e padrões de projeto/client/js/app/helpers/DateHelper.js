6fbac9ed5f01f1b3d2c3ae2ea64489ca05d506d9
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-03 09:35:14 -0300

    Fazendo o uso da expressão regular e validando se a data esta no formato correto

--- JavaScript: conhecendo o Browser e padrões de projeto/client/js/app/helpers/DateHelper.js
+++ JavaScript: conhecendo o Browser e padrões de projeto/client/js/app/helpers/DateHelper.js
@@ -12,6 +12,8 @@ class DateHelper {
 
     static textoParaData(texto) {
 
+            if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error("Deve estar no formato AAAA-MM-DD")
+
             return new Date(...texto.split("-")
             .map((item, indice)=>item - indice % 2));
 
