7d5e1ca547c50c7c0595e71a3caf93d245153580
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-04 18:36:07 -0300

    Deixando o formulario mais dinamico, por meio do "state"

--- Fundamentos de React: Escrevendo com TypeScript/alura-studies/src/components/Lista/index.tsx
+++ Fundamentos de React: Escrevendo com TypeScript/alura-studies/src/components/Lista/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './lista.module.scss';
 import Item from "./Item";
 
 function Lista() {
 
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     {
       tarefa: "React",
       tempo: "02:00:00",
@@ -17,15 +17,16 @@ function Lista() {
         tarefa: "TypeScript",
         tempo: "04:00:00",
     }
-  ];
+  ]);
 
   return (
     <aside className={style.listaTarefas}>
 
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}])}}>Estudos do dia</h2>
 
       <ul>
-       {tarefas.map((item, index) => (
+       {tarefas.map((item: JSX.IntrinsicAttributes & { tarefa: string; tempo: string; }, index: React.Key | null | undefined) => (
          <Item 
          key={index}
          {...item}         
