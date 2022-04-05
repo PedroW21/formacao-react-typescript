f8959d52a2a986be2d4b06a77ef5356ffa67a983
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-04-05 16:05:13 -0300

    Fim do curso

--- Fundamentos de React: Escrevendo com TypeScript/alura-studies/src/pages/App.tsx
+++ Fundamentos de React: Escrevendo com TypeScript/alura-studies/src/pages/App.tsx
@@ -1,19 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
-import style from './app.module.scss';
-import Cronometro from '../components/Cronometro';
 import { ITarefa } from '../types/tarefa';
+import style from './app.module.scss';
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
 
-  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
 
   return (
     <div className={style.AppStyle}>
-     <Formulario setTarefas= {setTarefas} />
-     <Lista tarefas={tarefas}/>
-     <Cronometro />
+      <Formulario setTarefas={setTarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
