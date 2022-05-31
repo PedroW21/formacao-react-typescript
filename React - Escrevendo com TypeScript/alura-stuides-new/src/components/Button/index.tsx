ada63064c1547e9a4dfddb7db9e9f6ebaa979c6a
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-05-31 17:45:15 -0300

    Our first component =)

---
+++ React - Escrevendo com TypeScript/alura-stuides-new/src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Button extends React.Component {
+    render()
+    {
+        return (
+            <>
+            <button>El boutao</button>
+            </>
+        )
+    }
+}
+
+export default Button;
