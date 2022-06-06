e49fa5b31d627eaa3ba72ec838f6f084b6ffe9a5
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-06-06 18:28:53 -0300

    Cronometro funcional no quesito de rodar o tempo (decrementar)

--- React - Escrevendo com TypeScript/alura-stuides-new/src/components/Button/index.jsx
+++ React - Escrevendo com TypeScript/alura-stuides-new/src/components/Button/index.jsx
@@ -4,9 +4,11 @@ import style from "./Button.module.scss";
 class Button extends React.Component {
     render()
     {
+        const {type = "button", onClick} = this.props;
+        
         return (
             <>
-            <button className={style.botao}>
+            <button className={style.botao} onClick={onClick} >
                 {this.props.children} 
             </button>
             </>
--- React - Escrevendo com TypeScript/alura-stuides-new/src/components/Cronometro/index.tsx
+++ React - Escrevendo com TypeScript/alura-stuides-new/src/components/Cronometro/index.tsx
@@ -20,6 +20,18 @@ export default function Cronometro({ selecionado } : Props) {
       setTempo(tempoParaSegundos((selecionado.tempo))) 
   }, [selecionado])
 
+  function regressiva(contador: number = 0)
+  {
+     setTimeout(() => {
+       if(contador > 0)
+       {
+         setTempo(contador-1);
+         return regressiva(contador-1);
+       }
+       
+     }, 1000)
+  }
+
   return (
     <div className={style.cronometro}>
     <p className={style.titulo}>
@@ -29,7 +41,7 @@ export default function Cronometro({ selecionado } : Props) {
       <div className={style.relogioWrapper}>
         <Relogio tempo= {tempo} />
       </div>
-        <Button>
+        <Button onClick={() => regressiva(tempo)}>
             Começar!
         </Button>
         
