48ddadd3115d23e72f26a937d2bde72daf279ab6
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-06-09 16:11:15 -0300

    Criando o buscador - importado alguns estilos prontos

--- React - Lidando com arquivos estaticos/aluroni/src/pages/Cardapio/index.tsx
+++ React - Lidando com arquivos estaticos/aluroni/src/pages/Cardapio/index.tsx
@@ -1,9 +1,15 @@
 import styles from "./Cardapio.module.scss";
 import {ReactComponent as Logo} from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
+
 export default function Cardapio()
 {
+    const [busca, setBusca] = useState("");
+
     return(
         <main>
+
             <nav className={styles.menu}>
                 <Logo />
                 <header className={styles.header}>
@@ -12,6 +18,17 @@ export default function Cardapio()
                     </div>
                 </header>
             </nav>
+
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                
+                <Buscador 
+                busca={busca} 
+                setBusca={setBusca}
+                />
+
+            </section>
+
         </main>
     )
 }
